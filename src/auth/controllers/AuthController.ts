2fabca51bf01865e2979526e457786717f391d37
lennoard <lennoardrai@gmail.com>
2022-08-17 23:11:51 -0300

    project architecture

---
+++ src/auth/controllers/AuthController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { Collection, Db, MongoClient } from "mongodb";
+import { User } from "../../domain/models/User";
+
+export class AuthController {
+  private client: MongoClient;
+  private db: Db;
+  private usesrsCollection: Collection<User>;
+
+  constructor() {
+    const uri = "mongodb://localhost:27017";
+    this.client = new MongoClient(uri);
+    this.db = this.client.db("socialapp");
+    this.usersCollection = this.db.collection<User>("users");
+  }
+
+  public signUp = async (request: Request, response: Response) => {
+    const { email, name, password } = request.body;
+
+    const newUser: User = { email, name, password };
+    const databaseUser = await this.usersCollection.findOne<User>({ email });
+
+    if (databaseUser) {
+      return response
+        .status(409)
+        .json({ error: "A user with this email address already exists" });
+    }
+
+    const result = await this.usersCollection.insertOne(newUser);
+
+    return response.status(200).json(result);
+  };
+
+  public signIn = async (request: Request, response: Response) => {
+    const { email, password } = request.body;
+
+    const databaseUser = await this.usersCollection.findOne<User>({
+      email,
+      password,
+    });
+
+    if (!databaseUser) {
+      return response.status(401).json({ error: "Invalid email or password" });
+    }
+
+    return response.json(databaseUser);
+  };
+}
